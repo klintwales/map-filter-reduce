6f5c71ad64caff43fe087dafc326cea1dbc77f96
Klint <klintwales@gmail.com>
2021-02-14 15:12:55 -0600

    Figured out how to access each item in an array then truncate the string

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-var numbers = [3, 56, 2, 48, 5];
+import emojipedia from "./emojipedia"
+//var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
 
@@ -9,3 +10,33 @@ var numbers = [3, 56, 2, 48, 5];
 //Find - find the first item that matches from an array.
 
 //FindIndex - find the index of the first item that matches.
+
+
+
+
+// const meaningsArray = emojipedia.map(function(num, index){
+//   return(emojipedia[index].meaning);
+// });
+//
+// const charArray = meaningsArray.map(function(meaning){
+//   return(meaning.substring(0,100));
+//
+// });
+
+//combined two commented out methods above. Finds all of the meaning sections of each element in each array and then shrinks them down to 100 characters
+const simplArray = emojipedia.map(function(num){
+  return(num.meaning.substring(0,100));
+});
+
+
+
+// console.log(meaningsArray);
+// console.log(charArray);
+console.log(simplArray);
+
+
+
+
+//access each meaning section of the array
+//take each meaning and shrink it down
+//add each slimmed down meaning to a new array
